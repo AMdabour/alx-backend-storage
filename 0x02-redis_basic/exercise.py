ecd6c401eae90dd5f37254957c97d9e003cadf19
a-dabour <ah910905@gmail.com>
2024-01-27 09:38:35 +0200

    update exercise

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ creating a cache class and storing
 an instance of Redis client in it
 """
 from redis import Redis
-from typing import Union
+from typing import Union, Optional, Callable
 from uuid import uuid4
 
 
@@ -21,3 +21,21 @@ class Cache():
         key: str = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """convert the data back to the desired format"""
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """convert data to string format"""
+        value = self._redis.get(key).decode("utf-8")
+        return value
+
+    def get_int(self, key: str) -> int:
+        """convert data to integer format"""
+        value = int(self._redis.get(key).decode("utf-8"))
+        return value
